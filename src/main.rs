ab7aececab9569837451f8ab88261a8a2ba257bf
Karl <local-part@domain>
2022-12-26 23:11:59 +0100

    [G7TIDreversecalculator/ updated to version 1.21:] (src/)main.rs updated: proofread the source code.

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@
 extern crate libm;
 
 use std::env;
-use std::io::*; //use std::io::{stdin,stdout,Write};
+use std::io::*;
 use std::str;
 use rand::Rng;
 use num_traits::pow;
@@ -49,39 +49,38 @@ fn function_length_number( number: u32, radix: i8 ) -> u8
 
 fn function_random_G7TID( integer_G7TID_target: i32 ) -> ( i16, i32, i8, i16, u16, u16 )
 {
-	let maximum_16bitunsigned: u16 /*= ( pow( 2, 16 ) - 1 ) as u16*/;
-	let mut integer_TID: u16 /*= 0*/;
-	let mut integer_SID: u16 /*= 0*/;
-	let mut result: u16 /*= 0*/;
-	let mut integer_TSV: i16 /*= 0*/;
-	let mut integer_TRV: i8 /*= 0*/;
-	let mut integer_IDNo: u32 /*= 0*/;
-	let million: i32 /*= pow( 10, 6 ) as i32*/;
-	let mut integer_G7TID: i32 /*= 0*/;
-	let radix: i8 /*u8 = 10*/;
-	let maximum_32bitunsigned: u32 /*= ( pow( 2, 32 ) - 1 ) as u32*/;
-	let mut length_maximum: u8 /*= 10*/;
-	let length_G7TID: u8 /*= 6*/;
-	let mut length_G7SID: u8 /*= 4*/;
-	let mut integer_G7SID: i16 /*= 0*/;
-	let mut boolean_flag: bool /*= false*/;
+	let maximum_16bitunsigned: u16;
+	let mut integer_TID: u16;
+	let mut integer_SID: u16;
+	let mut result: u16;
+	let mut integer_TSV: i16;
+	let mut integer_TRV: i8;
+	let mut integer_IDNo: u32;
+	let million: i32;
+	let mut integer_G7TID: i32;
+	let radix: i8;
+	let maximum_32bitunsigned: u32;
+	let mut length_maximum: u8;
+	let length_G7TID: u8;
+	let mut length_G7SID: u8;
+	let mut integer_G7SID: i16;
+	let mut boolean_flag: bool;
 
-	maximum_16bitunsigned = u16::MAX /*( pow( 2, 16 ) - 1 ) as u16*/;
+	maximum_16bitunsigned = u16::MAX /*( pow( 2, 16 ) - 1 ) as u16*/; // My code basically brute-forces a G7TID. POW and the likes will only slow it down.
 	million = pow( 10, 6 ) as i32;
 	length_G7TID = 6;
 	radix = 10;
 	maximum_32bitunsigned = u32::MAX /*( pow( 2, 32 ) - 1 ) as u32*/;
 	length_maximum = function_length_number( maximum_32bitunsigned, radix );
-	integer_TID = rand::thread_rng().gen_range( 0..maximum_16bitunsigned ); // My code basically brute-forces a G7TID. POW and the likes will only slow it down.
+	integer_TID = rand::thread_rng().gen_range( 0..maximum_16bitunsigned );
 	boolean_flag = false;
-	while ( boolean_flag == false ) // Wait a minute. I could've come up with better. Oh, well.
+	while ( boolean_flag == false ) // Wait a minute. I could have come up with better. Oh, well.
 	{
 		integer_SID = 0;
 		while ( integer_SID < maximum_16bitunsigned && boolean_flag == false )
 		{
-			//dbg!( integer_TID, integer_SID, maximum_16bitunsigned, maximum_16bitunsigned as u32 + 1u32, integer_TID as u32 + ( maximum_16bitunsigned as u32 + 1u32 ) * integer_SID as u32, ( integer_TID as u32 + ( maximum_16bitunsigned as u32 + 1u32 ) * integer_SID as u32 ) % million as u32 ); //cd bin/G7TIDreversecalculator && cargo build && cargo build --release && cd; cls; while true; do s_0=$(./bin/G7TIDreversecalculator/target/debug/g7tidbc $(( ( $[RANDOM%65535] + ( 2**16 * $[RANDOM%65535] ) ) % 10**6 )) | tail -r); echo "${s_0}"; echo; i_TID=$(echo "${s_0}" | grep -Eo '^TID:\t[0-9]+' | rev | cut -d: -f1 | rev | xargs); i_SID=$(echo "${s_0}" | grep -Eo '^SID:\t[0-9]+' | rev | cut -d: -f1 | rev | xargs); s_1=$(./bin/G7TIDcalculator/bin/g7tidc ${i_TID} ${i_SID}); diff --suppress-common-lines -y <(echo "${s_0}") <(echo "${s_1}"); done
 			integer_IDNo = integer_TID as u32 + ( ( maximum_16bitunsigned as u32 + 1u32 ) * integer_SID as u32 ) as u32;
-			integer_G7TID = ( integer_IDNo % million as u32 ) as i32; // My code basically brute-forces a G7TID. POW here will only slow it down further.
+			integer_G7TID = ( integer_IDNo % million as u32 ) as i32;
 			if ( integer_G7TID == integer_G7TID_target )
 			{
 				length_G7SID = length_maximum - length_G7TID;
@@ -89,34 +88,29 @@ fn function_random_G7TID( integer_G7TID_target: i32 ) -> ( i16, i32, i8, i16, u1
 				result = ( integer_TID /*( integer_IDNo >> 16 )*/ ^ integer_SID /*( integer_IDNo & 0xFFFF )*/ ) as u16;
 				integer_TRV = ( result & 0xF ) as i8;
 				integer_TSV = ( result >> 4 ) as i16;
-				boolean_flag = true; // Cleanly break out of that control flow statement. Well, cleanly...
-				return ( integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID ); // Never mind. Just return.
+				boolean_flag = true; // Break out of this control flow statement.
+				return ( integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID ); // Return.
 			}
 			integer_SID += 1;
 		}
 		integer_SID -= 1;
-		integer_TID = rand::thread_rng().gen_range( 0..maximum_16bitunsigned ); // If I didn't find a valid G7TID, reroll.
+		integer_TID = rand::thread_rng().gen_range( 0..maximum_16bitunsigned ); // If I did not find a valid G7TID, reroll.
 	}
 
 	return ( 0, 0, 0, 0, 0, 0 ); //return ( integer_G7TID, integer_SID, integer_TID ); // Return at least 0, 0, 0, 0, 0 and 0, because RustC expect an n-tuple of integer numbers, or else it will not compile at all.
-} // Boy, what a sight. Shock, horror. I gotta rewrite that abomination. Yes, I am a software engineer apparently. LOL, LMAO etc.
+} // I have to optimize that function.
 
 fn main()
 {
 	let vector_argument: Vec<String> = env::args().collect();
-	//dbg!(vector_argument);
-	//let mut string_input = String::new();
-	let integer_G7TID_target: i32 = vector_argument[ 1 ].trim().to_string().parse::<i32>().unwrap(); //let mut integer_targetG7TID: u32 = 0;
-	//dbg!( integer_targetG7TID );
-	//let mut boolean_G7TIDexistence: bool = false;
-	//let mut boolean_G7TIDvalidity: bool = false;
+	let integer_G7TID_target: i32 = vector_argument[ 1 ].trim().to_string().parse::<i32>().unwrap();
 
-	/*if ( ! integer_targetG7TID.parse::<u32>().is_ok() )
+	/*if ( ! integer_G7TID_target.parse::<u32>().is_ok() )
 	{
 		eprintln!("Error: Invalid Target Value\nThe input value is not an integer number.");
-		std::process::exit(1);
-	}*/ // .trim().to_string().parse::<u32>().unwrap() already converted the string to an unsigned integer. I don't need to call Parse. The Rust compiler knows variable types at compile time. I thus don't need to check their types.
-	if ( integer_G7TID_target < 0 || 999999 < integer_G7TID_target ) // Although I don't need to check for negative values, since integer_targetG7TID is an unsigned integer.
+		std::process::exit( 1 );
+	}*/ // .trim().to_string().parse::<u32>().unwrap() already converted the string to an unsigned integer. I do not need to call Parse. The Rust compiler knows variable types at compile time. I therefore do not need to check their types.
+	if ( integer_G7TID_target < 0 || 999999 < integer_G7TID_target ) // Although I do not need to check for negative values, since integer_G7TID_target is an unsigned integer.
 	{
 		eprintln!("Error: Invalid Target Value\nThe input value is not an integer number between 0 and 999999.");
 		std::process::exit( 1 );
@@ -127,13 +121,13 @@ fn main()
 	stdin().read_line(&mut string_input)
 		.expect("Error: Invalid Input");
 	integer_targetG7TID = string_input.trim().to_string()
-		.parse::<u32>().unwrap();*/ // If I gotta ask the user for a value, I should make a GUI application, not ship it in some shitty interactive-only CLI binary. That's why we write read-me files and manuals for fuck's sake. Don't insult your users' intelligence.
+		.parse::<u32>().unwrap();*/ // If I gotta ask the user for a value, I should make a GUI application, not ship that in an interactive CLI binary. Here is why we write read-me files and manuals for fuck's sake. Do not insult your users' intelligence.
 	let ( integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID ) = function_random_G7TID( integer_G7TID_target );
 	println!("G7SID:\t{}\nG7TID:\t{}\nTRV:\t{}\nTSV:\t{}\nSID:\t{}\nTID:\t{}", integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID );
-} // Rust is a wonder. I'm doing my career with it!
+} // Rust is a wonder. I am doing my career with it!
 
 /*//	main.rs
-////	G8TID Reverse Calculator
+////	G7TID Reverse Calculator
 ////
 ////	Karl V. P. B. `kvpb` AKA Karl Thomas George West `ktgw`
 ////	+1 (DDD) DDD-DDDD
@@ -143,8 +137,4 @@ fn main()
 ////	https://www.linkedin.com/in/
 ////	https://twitter.com/ktgwkvpb
 ////	https://github.com/kvpb
-////	https://vm.tiktok.com/ZSwAmcFh/
-////
-////	I gotta get my hands dirty. Those five tarés from the PKHEX community are obnoxious, more toxic than radioactive waste. If they were helpful at least, I wouldn't say, but they are as useful as square wheels, know nothing, talk about everything. Just five more baltringues. But this is just the usual Reddit-Twitch-Discord clientele, so it doesn't surprise me much. I shouldn't care about them. Let's keep finding out how these games work right now.
-////	This algorithm applies to G7TID and G8TID alike, and I bet it will for G9TID too. The question is how do I know all _possible_ values for a G8TID _from a cryptographically secure game_? If they encrypt the seed, ~~or if they just encrypt any part of the process,~~ how can I tell whether I can get any possible value from the above formula? If the maximum value is 65,536 + ( 65,536 * 65,536 ), 4,295,032,832 G8TID theoretically exist. LGPE, SS and LA all rely on nn::crypto::GenerateCryptographicallyRandomBytes. Good luck with that.
-*///	Lifelike's Heatwave and Motion are nasty bangers by the way. Sounds like Paolo Del Prete is behind somehow. He seemed to enjoy toying around with Stop like that. Gotta keep spinning that stuff louder on the highway. Heh, I miss my ride. Which motorbike should I get in North America by the way? I haven't thought about it. Let's start with something sturdy and reliable, nothing too pompous. And when I get near a racing track, I'm getting myself a Hayabusa.
+*///	https://vm.tiktok.com/ZSwAmcFh/
