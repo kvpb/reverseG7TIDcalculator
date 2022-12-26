016af110d35d294b940a0918789fab8e8854dbea
Karl <local-part@domain>
2022-12-26 23:10:42 +0100

    [G7TIDreversecalculator/ updated to version 1.20:] (src/)main.rs updated: support outputting TSV and TRV.

--- src/main.rs
+++ src/main.rs
@@ -47,28 +47,31 @@ fn function_length_number( number: u32, radix: i8 ) -> u8
 	return length;
 }
 
-fn function_random_G7TID( integer_G7TID_target: i32 ) -> ( i16, i32, u16, u16 )
+fn function_random_G7TID( integer_G7TID_target: i32 ) -> ( i16, i32, i8, i16, u16, u16 )
 {
 	let maximum_16bitunsigned: u16 /*= ( pow( 2, 16 ) - 1 ) as u16*/;
 	let mut integer_TID: u16 /*= 0*/;
 	let mut integer_SID: u16 /*= 0*/;
+	let mut result: u16 /*= 0*/;
+	let mut integer_TSV: i16 /*= 0*/;
+	let mut integer_TRV: i8 /*= 0*/;
 	let mut integer_IDNo: u32 /*= 0*/;
 	let million: i32 /*= pow( 10, 6 ) as i32*/;
 	let mut integer_G7TID: i32 /*= 0*/;
-	let mut boolean_flag: bool /*= false*/;
-	let length_G7TID: u8 /*= 6*/;
 	let radix: i8 /*u8 = 10*/;
 	let maximum_32bitunsigned: u32 /*= ( pow( 2, 32 ) - 1 ) as u32*/;
-	let mut length_max: u8 /*= 10*/;
+	let mut length_maximum: u8 /*= 10*/;
+	let length_G7TID: u8 /*= 6*/;
 	let mut length_G7SID: u8 /*= 4*/;
 	let mut integer_G7SID: i16 /*= 0*/;
+	let mut boolean_flag: bool /*= false*/;
 
 	maximum_16bitunsigned = u16::MAX /*( pow( 2, 16 ) - 1 ) as u16*/;
 	million = pow( 10, 6 ) as i32;
 	length_G7TID = 6;
 	radix = 10;
 	maximum_32bitunsigned = u32::MAX /*( pow( 2, 32 ) - 1 ) as u32*/;
-	length_max = 10;
+	length_maximum = function_length_number( maximum_32bitunsigned, radix );
 	integer_TID = rand::thread_rng().gen_range( 0..maximum_16bitunsigned ); // My code basically brute-forces a G7TID. POW and the likes will only slow it down.
 	boolean_flag = false;
 	while ( boolean_flag == false ) // Wait a minute. I could've come up with better. Oh, well.
@@ -76,15 +79,18 @@ fn function_random_G7TID( integer_G7TID_target: i32 ) -> ( i16, i32, u16, u16 )
 		integer_SID = 0;
 		while ( integer_SID < maximum_16bitunsigned && boolean_flag == false )
 		{
+			//dbg!( integer_TID, integer_SID, maximum_16bitunsigned, maximum_16bitunsigned as u32 + 1u32, integer_TID as u32 + ( maximum_16bitunsigned as u32 + 1u32 ) * integer_SID as u32, ( integer_TID as u32 + ( maximum_16bitunsigned as u32 + 1u32 ) * integer_SID as u32 ) % million as u32 ); //cd bin/G7TIDreversecalculator && cargo build && cargo build --release && cd; cls; while true; do s_0=$(./bin/G7TIDreversecalculator/target/debug/g7tidbc $(( ( $[RANDOM%65535] + ( 2**16 * $[RANDOM%65535] ) ) % 10**6 )) | tail -r); echo "${s_0}"; echo; i_TID=$(echo "${s_0}" | grep -Eo '^TID:\t[0-9]+' | rev | cut -d: -f1 | rev | xargs); i_SID=$(echo "${s_0}" | grep -Eo '^SID:\t[0-9]+' | rev | cut -d: -f1 | rev | xargs); s_1=$(./bin/G7TIDcalculator/bin/g7tidc ${i_TID} ${i_SID}); diff --suppress-common-lines -y <(echo "${s_0}") <(echo "${s_1}"); done
 			integer_IDNo = integer_TID as u32 + ( ( maximum_16bitunsigned as u32 + 1u32 ) * integer_SID as u32 ) as u32;
 			integer_G7TID = ( integer_IDNo % million as u32 ) as i32; // My code basically brute-forces a G7TID. POW here will only slow it down further.
 			if ( integer_G7TID == integer_G7TID_target )
 			{
-				length_max = function_length_number( maximum_32bitunsigned, radix );
-				length_G7SID = length_max - length_G7TID;
-				integer_G7SID = floor( ( integer_IDNo / pow( 10, ( length_max - length_G7SID ).into() ) ).into() ) as i16;
+				length_G7SID = length_maximum - length_G7TID;
+				integer_G7SID = floor( ( integer_IDNo / pow( 10, ( length_maximum - length_G7SID ).into() ) ).into() ) as i16;
+				result = ( integer_TID /*( integer_IDNo >> 16 )*/ ^ integer_SID /*( integer_IDNo & 0xFFFF )*/ ) as u16;
+				integer_TRV = ( result & 0xF ) as i8;
+				integer_TSV = ( result >> 4 ) as i16;
 				boolean_flag = true; // Cleanly break out of that control flow statement. Well, cleanly...
-				return ( integer_G7SID, integer_G7TID, integer_SID, integer_TID ); // Never mind. Just return.
+				return ( integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID ); // Never mind. Just return.
 			}
 			integer_SID += 1;
 		}
@@ -92,7 +98,7 @@ fn function_random_G7TID( integer_G7TID_target: i32 ) -> ( i16, i32, u16, u16 )
 		integer_TID = rand::thread_rng().gen_range( 0..maximum_16bitunsigned ); // If I didn't find a valid G7TID, reroll.
 	}
 
-	return ( 0, 0, 0, 0 ); //return ( integer_G7TID, integer_SID, integer_TID ); // Return at least 0, 0 and 0, because RustC expect a tuple ( i32, i32, i32 ), or else it will not compile at all.
+	return ( 0, 0, 0, 0, 0, 0 ); //return ( integer_G7TID, integer_SID, integer_TID ); // Return at least 0, 0, 0, 0, 0 and 0, because RustC expect an n-tuple of integer numbers, or else it will not compile at all.
 } // Boy, what a sight. Shock, horror. I gotta rewrite that abomination. Yes, I am a software engineer apparently. LOL, LMAO etc.
 
 fn main()
@@ -122,8 +128,8 @@ fn main()
 		.expect("Error: Invalid Input");
 	integer_targetG7TID = string_input.trim().to_string()
 		.parse::<u32>().unwrap();*/ // If I gotta ask the user for a value, I should make a GUI application, not ship it in some shitty interactive-only CLI binary. That's why we write read-me files and manuals for fuck's sake. Don't insult your users' intelligence.
-	let ( integer_G7SID, integer_G7TID, integer_SID, integer_TID ) = function_random_G7TID( integer_G7TID_target );
-	println!("G7SID:\t{}\nG7TID:\t{}\nSID:\t{}\nTID:\t{}", integer_G7SID, integer_G7TID, integer_SID, integer_TID );
+	let ( integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID ) = function_random_G7TID( integer_G7TID_target );
+	println!("G7SID:\t{}\nG7TID:\t{}\nTRV:\t{}\nTSV:\t{}\nSID:\t{}\nTID:\t{}", integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID );
 } // Rust is a wonder. I'm doing my career with it!
 
 /*//	main.rs
